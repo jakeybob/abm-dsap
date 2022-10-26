f3b10e51a1175d0d04363754f3c7972925efb821
Bob Taylor <j.boaby@gmail.com>
2022-10-26 21:20:32 +0100

    testing OSMMakie to extract/plot

--- code/04-OSM-SIR/04-OSM-map-downloads.jl
+++ code/04-OSM-SIR/04-OSM-map-downloads.jl
@@ -1,11 +1,44 @@
 cd(@__DIR__)
+import Pkg
+Pkg.activate(".")
 import Downloads
+
+using LightOSM, OSMMakie, GLMakie
 
 try
     mkdir("maps")
 catch
     @warn "maps folder already exists"
 end
+
+# https://github.com/MakieOrg/OSMMakie.jl
+# define area boundaries
+area = (
+    minlat = 55.85599, minlon = -4.24921, # bottom left corner
+    maxlat = 55.86071, maxlon = -4.23719 # top right corner
+)
+
+
+# download OpenStreetMap data
+download_osm_network(:bbox; # rectangular area
+    area..., # splat previously defined area boundaries
+    network_type = :drive, # download motorways
+    save_to_file_location = "maps/test.json"
+);
+
+# load as OSMGraph
+osm = graph_from_file("maps/test.json";
+    graph_type = :light, # SimpleDiGraph
+    weight_type = :distance
+)
+
+# use min and max latitude to calculate approximate aspect ratio for map projection
+autolimitaspect = map_aspect(area.minlat, area.maxlat)
+
+# plot it
+fig, ax, plot = osmplot(osm; axis = (; autolimitaspect))
+fig
+
 
 # George Square, Glasgow
 # OSM format
