a0367b6ce67ca89c0619055815584b242c6c7a6c
Bob Taylor <j.boaby@gmail.com>
2022-11-27 21:03:59 +0000

    tidied up journey type choice

--- code/03-graphgrid-SIR/03-graphgrid-SIR-2.jl
+++ code/03-graphgrid-SIR/03-graphgrid-SIR-2.jl
@@ -30,9 +30,9 @@ room = permutedims(BitArray([
     0 1 1 1 1 1 1 1 1 1 0;
     0 0 0 0 0 0 0 0 0 0 0;]))
 
-bathroom_pos = (8, 3)
-kitchen_pos = (8, 2)
-exit_pos = (9, 2)
+bathroom_pos = (10, 2)
+kitchen_pos = (2, 2)
+exit_pos = (10, 9)
 
 function init_model(bit_space;
     # agent properties
@@ -52,14 +52,16 @@ function init_model(bit_space;
     seed = 1234,
     bathroom_weight = 0.05,
     kitchen_weight = 0.05,
-    exit_weight = 0.01
+    exit_weight = 0.01,
+    journey_weights = Weights([bathroom_weight, kitchen_weight, exit_weight, 1 - (bathroom_weight + kitchen_weight + exit_weight)])
 )
     # dictionary of above properties to be applied globally to model
     properties = Dict(:infection_period => infection_period, 
         :reinfection_probability => reinfection_probability, 
         :death_rate => death_rate,
         :interaction_radius => interaction_radius,
-        :Δt => Δt)
+        :Δt => Δt,
+        :journey_weights => journey_weights)
 
     space = GridSpace(size(bit_space); periodic = false)
     pathfinder = AStar(space; walkmap = bit_space, diagonal_movement = true)
@@ -70,7 +72,12 @@ function init_model(bit_space;
     cols = room_size[1]
     rows = room_size[2]
     num_points = rows*cols
-    valid_spaces = (bit_space .== 1)[1:num_points]
+    # valid_spaces = (bit_space .== 1)[1:num_points]
+    valid_spaces = (bit_space .== 1)
+    valid_spaces[bathroom_pos[1], bathroom_pos[2]] = false
+    valid_spaces[kitchen_pos[1], kitchen_pos[2]] = false
+    valid_spaces[exit_pos[1], exit_pos[2]] = false
+    valid_spaces = valid_spaces[1:num_points]
     available_spaces = (1:num_points)[valid_spaces]
     positions_to_use = sample(available_spaces, N; replace=false)
     index2tuple(pos) = (rem(pos, cols), div(pos, cols)+1)
@@ -85,20 +92,28 @@ function init_model(bit_space;
     return model, pathfinder
 end 
 
-
 function agent_step!(agent, model)
-    choice = rand()
-    if agent.journey_type == :none && (choice < 0.1)
-        agent.journey_type = :bathroom
-        plan_route!(agent, (8, 3), pathfinder)
+    # if at desk then choose to either stay there or start a journey
+    # if journey chosen, then plot route
+    if agent.journey_type == :none
+        agent.journey_type = sample([:bathroom, :kitchen, :exit, :none], model.properties[:journey_weights])
+        if agent.journey_type == :bathroom
+            plan_route!(agent, bathroom_pos, pathfinder)
+        elseif agent.journey_type == :kitchen
+            plan_route!(agent, kitchen_pos, pathfinder)
+        elseif agent.journey_type == :exit
+            plan_route!(agent, exit_pos, pathfinder)
+        end
     end
 
-    # if reached bathroom, thehn head back
-    if agent.journey_type == :bathroom && agent.pos == (8, 3)
+    # if reached destination, then head back
+    if (agent.journey_type == :bathroom && agent.pos == bathroom_pos) |
+        (agent.journey_type == :kitchen && agent.pos == kitchen_pos) |
+        (agent.journey_type == :exit && agent.pos == exit_pos)
         plan_route!(agent, agent.pos_initial, pathfinder)
     end
 
-    # if on a journey, then move. If arrived back home, then not on a journey.
+    # if on a journey, then move. If arrived back home, then not on a journey
     if agent.journey_type != :none
         move_along_route!(agent, model, pathfinder)
         if agent.pos == agent.pos_initial
@@ -106,8 +121,6 @@ function agent_step!(agent, model)
         end
     end
 end
-
-
 
 GLMakie.activate!()
 colours(agent) = agent.status == :S ? "#0000ff" : agent.status == :I ? "#ff0000" : "#00ff00"
@@ -118,3 +131,4 @@ fig, ax, abmobs = abmplot(model;
     ac = colours,
     heatarray = _ -> pathfinder.walkmap)
 fig
+
